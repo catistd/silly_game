fea0a1d7616c4dd4b48fd9060df766004396a161
ciander <ciander@proton.me>
2025-06-22 19:49:02 -0700

    even more projectile work

--- Scripts/classes/ProjectileBase.gd
+++ Scripts/classes/ProjectileBase.gd
@@ -4,3 +4,21 @@ class_name ProjectileBase
 @export_category("Bullet Stats")
 @export var damage: int
 @export var lifetime: float
+@export var persistant: bool
+
+func _ready() -> void:
+    self.add_to_group("projectile")
+
+    var timer := Timer.new()
+    add_child(timer)
+
+    timer.wait_time = lifetime
+    timer.one_shot = true
+
+    timer.timeout.connect(Callable(self, "LifetimeEnd"))
+
+    timer.start()
+
+
+func LifetimeEnd() -> void:
+    queue_free()
